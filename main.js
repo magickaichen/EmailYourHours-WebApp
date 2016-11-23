23c86126687203906f9328a21883044f52259eb6
magickaichen <xiaokaic@msu.edu>
2016-11-23 15:55:20 -0500

    web design and framework (early version)

---
+++ main.js
@@ -0,0 +1,14 @@
+var weekdayApp = new Vue({
+  el: '#weekdayApp',
+  data: {
+    weekdays:[
+      { text: 'Monday' },
+      { text: 'Tuesday' },
+      { text: 'Wednesday' },
+      { text: 'Thursday' },
+      { text: 'Friday' },
+      { text: 'Saturday' },
+      { text: 'Sunday' },
+    ]
+  }
+})
